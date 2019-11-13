4af23d965a729b227141311e2daec1da1724c167
Shreyas <shreyaschorge81@gmail.com>
2019-11-13 14:55:37 +0530

    Basic Clean Up, Setup Router and Link, About Page

---
+++ client/src/components/pages/About.jsx
@@ -0,0 +1,15 @@
+import React, { Fragment } from "react";
+
+const About = () => {
+  return (
+    <Fragment>
+      <h1>About</h1>
+      <p className="my-1">Full Stack Contact Keeper application</p>
+      <p className="my-1">
+        <strong>Version </strong>1.0.0
+      </p>
+    </Fragment>
+  );
+};
+
+export default About;
